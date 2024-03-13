472d62d01c6c9d3c91b409f8b3510a679637f9bf
Татьяна <ltasya72@gmail.com>
2024-03-13 09:48:49 +0200

    верстка слайдера surf

--- gulpfile.js
+++ gulpfile.js
@@ -114,7 +114,7 @@ function images() {
       .pipe(newer("images"))
       .pipe(imagemin())
 
-      .pipe(dest("images/header/"))
+      .pipe(dest("images/surf/"))
   );
 }
 
--- js/main.js
+++ js/main.js
@@ -15,4 +15,30 @@ $(function () {
     sliderTOScroll: 4,
     asNavFor: ".header__slider",
   });
+
+  // $(".surf__slider").slick({
+  //   sliderToShow: 4,
+  //   sliderTOScroll: 1,
+  //   nextArrow:
+  //     '<button class="slick-arrow slick-next" aria-label="Next"><svg width="24" height="20"><use href="./images/icons/icons.svg#arrow-right-next"></use></button>',
+  //   prevArrow:
+  //     '<button class="slick-arrow slick-prev" aria-label="Previous"><svg width="24" height="20"><use href="./images/icons/icons.svg#arrow-left-prev"></use></button>',
+  // });
+});
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
